cfe69a4e0fa34cbd6799f5231ca29df5eb3208d0
Naresh Chudasama <112629859+nkc2887Dev@users.noreply.github.com>
2025-06-02 17:36:46 +0530

    Update processEnv.config.ts

--- src/config/processEnv.config.ts
+++ src/config/processEnv.config.ts
@@ -1,6 +1,6 @@
 const config = {
   MONGO: {
-    URL: process.env.MONGO_URL || "mongodb://127.0.01:27017/inventory-management"
+    URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/inventory-management"
   },
   PORT: process.env.PORT || "9876",
 };
